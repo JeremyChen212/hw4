b34196ca4e3e70109002077475dbddc3c65e2ad5
JeremyChen212 <50683386+JeremyChen212@users.noreply.github.com>
2023-05-01 19:21:45 -0700

    update

--- server-worker.js
+++ server-worker.js
@@ -1,9 +1,9 @@
 var CACHE_VERSION = 'myapp-v1';
 var CACHE_FILES = [
-    'images/lightblue.jpg',
-    'images/lightgold.jpg',
-    'app.js',
-    'style.css'
+    '/hw4/images/lightblue.jpg',
+    '/hw4/images/lightgold.jpg',
+    '/hw4/app.js',
+    '/hw4/style.css'
 ];
 
 self.addEventListener('install', event => {
