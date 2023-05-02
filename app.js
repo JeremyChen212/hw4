135391b315345f243ef2033c0870282abc2090b0
JeremyChen212 <50683386+JeremyChen212@users.noreply.github.com>
2023-05-01 19:08:36 -0700

    js function change theme

--- app.js
+++ app.js
@@ -7,3 +7,21 @@ if (navigator.serviceWorker) {
 } else {
     console.log('Service Worker is not supported in this browser.')
 }
+
+const colorSelect = document.getElementById("colorSelect")
+const value = colorSelect.value
+const lightbluebg = document.getElementById("lightblue")
+const lightgoldbg = document.getElementById("lightgold")
+
+colorSelect.addEventListener("change", handleSelectChange)
+
+
+function handleSelectChange(event) {
+    const currentValue = event.target.value;
+    console.log(currentValue)
+    changeTheme(currentValue)
+}
+function changeTheme(color) {
+    console.log("changetheme")
+    document.getElementsByTagName('body')[0].style.backgroundImage = 'url(/images/' + color + '.jpg)';
+}
